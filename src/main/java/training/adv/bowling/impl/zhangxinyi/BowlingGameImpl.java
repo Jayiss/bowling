d7415d042fbbc98db865b81693a7dff165af2d68
alo1719 <1335250440@qq.com>
2019-10-08 14:03:45 +0800

    implements BowlingGameImpl

--- src/main/java/training/adv/bowling/impl/zhangxinyi/BowlingGameImpl.java
+++ src/main/java/training/adv/bowling/impl/zhangxinyi/BowlingGameImpl.java
@@ -3,30 +3,52 @@ package training.adv.bowling.impl.zhangxinyi;
 import training.adv.bowling.api.*;
 import training.adv.bowling.impl.AbstractGame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class BowlingGameImpl extends AbstractGame<BowlingTurn, BowlingRule> implements BowlingGame {
 
-    public BowlingGameImpl(BowlingRule rule) {
+    private Integer totalScore;
+    private List<Integer> scores;
+    private List<BowlingTurn> turns;
+    private BowlingRule rule;
+
+    BowlingGameImpl(BowlingRule rule) {
         super(rule);
+        totalScore = 0;
+        scores = new ArrayList<Integer>();
+        turns = new ArrayList<BowlingTurn>();
+        this.rule = rule;
     }
 
     @Override
     public Integer getTotalScore() {
-        return null;
+        return totalScore;
     }
 
     @Override
     public Integer[] getScores() {
-        return new Integer[0];
+        return scores.toArray(new Integer[0]);
     }
 
     @Override
     public BowlingTurn[] getTurns() {
-        return new BowlingTurn[0];
+        return turns.toArray(new BowlingTurn[0]);
     }
 
     @Override
     public Integer[] addScores(Integer... pins) {
-        return new Integer[0];
+        BowlingTurn[] turnsArray = turns.toArray(new BowlingTurn[0]);
+        if (!rule.isGameFinished(turnsArray)) {
+            if (rule.isNewPinsAllowed(turnsArray, pins)) {
+                turns = Arrays.asList(rule.addScores(turnsArray, pins));
+                turnsArray = turns.toArray(new BowlingTurn[0]);
+                scores = Arrays.asList(rule.calcScores(turnsArray));
+            }
+        }
+        return getScores();
     }
 
     @Override
