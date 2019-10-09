9f2e7d9cd68ce6e0086b15b5a48def38a9ae65aa
caoyu <caoyupersonal@hotmail.com>
2019-10-09 14:16:54 +0800

    Merged commits from Jayiss

--- src/main/java/training/adv/bowling/impl/caoyu/BowlingGameFactoryImpl.java
+++ src/main/java/training/adv/bowling/impl/caoyu/BowlingGameFactoryImpl.java
@@ -2,8 +2,6 @@ package training.adv.bowling.impl.caoyu;
 
 import training.adv.bowling.api.BowlingGame;
 import training.adv.bowling.api.BowlingGameFactory;
-import training.adv.bowling.api.BowlingTurn;
-import training.adv.bowling.api.GameEntity;
 
 public class BowlingGameFactoryImpl implements BowlingGameFactory {
 
